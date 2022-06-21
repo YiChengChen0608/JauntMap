08452921fb2a46ef70d7fb437b1309ffe614d403
YiCheng <49583905+YiChengChen0608@users.noreply.github.com>
2022-06-21 23:47:17 +0800

    Update src/App.tsx
    Co-authored-by: LynnChien <63957817+LynnChien@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/global'
-import Layout from './layout/default'
+import Layout from './layout/Layout'
 
 function App (): React.ReactElement {
   return (
