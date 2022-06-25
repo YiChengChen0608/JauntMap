a2c00c12f23e9e423903bafe6f1a676a868d8a11
YiCheng <dekknm0971@gmail.com>
2022-06-25 22:42:44 +0800

    feat: tab menu add rwd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 import { Props } from '../../utils/types'
 import { ReactComponent as Logo } from '../assets/images/logo.svg'
-import Tab from './Tab'
+import { ReactComponent as Setting } from '../assets/images/setting.svg'
+import { ReactComponent as DarkMode } from '../assets/images/dark_mode.svg'
+import StyledTab from './Tab'
 
 function Header ({ className, children }: Props): React.ReactElement {
+  const [menuIsOpen, setMenuIsOpen] = React.useState<Boolean>(false)
+
+  const setMenuStatus: React.MouseEventHandler<HTMLButtonElement> = React.useCallback(() => {
+    return setMenuIsOpen((previousState) => !previousState)
+  }, [])
   return (
     <header className={className}>
       <div className="header-logo">
         <Logo className="header-logo-image" />
       </div>
-      {children}
-      <Tab />
+      <div className="header-middle">
+        {children}
+      </div>
+      <div className="header-tab">
+        <StyledTab className="header-menu" />
+        <div className="header-submenu">
+          <button className={`btn ${menuIsOpen ? 'btn-active' : ''}`} type='button' onClick={setMenuStatus}><Setting className="btn-icon" /></button>
+          <div className={`header-submenu-popup ${menuIsOpen ? 'popup-active' : ''}`}>
+            <button className="btn" type='button'><DarkMode className="btn-icon" /></button>
+            <button className="btn-logout" type='button'>登出</button>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
@@ -23,5 +41,56 @@ const StyledHeader = styled(Header)<Props>`
   grid-template-columns: 1fr 2fr 1fr;
   align-items: center;
   padding: 18px 40px;
+  .header {
+    &-middle {
+      justify-self: center;
+    }
+    &-tab {
+      justify-self: flex-end;
+      display: flex;
+      gap: 10px;
+    }
+    &-menu {
+      @media (max-width: ${({ theme }) => theme.device.tablet}) {
+        display: none;
+      }
+    }
+    &-submenu {
+      position: relative;
+      &-popup {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        max-height: 0;
+        border-radius: 6px;
+        padding: 0 8px;
+        margin-top: .5rem;
+        background: ${({ theme }) => theme.colors.background};
+        box-shadow: ${({ theme }) => theme.effects.card_shadow};
+        position: absolute;
+        right: 50%;
+        transform: translateX(50%);
+        transition: all 0.3s ease-out;
+        opacity: 0;
+        .btn-logout {
+          border: none;
+          background: transparent;
+          letter-spacing: 0.2em;
+          padding: 0;
+          color: ${({ theme }) => theme.colors.blue_green};
+          font-style: ${({ theme }) => theme.texts.font_style};
+          font-weight: ${({ theme }) => theme.texts.font_weigth};
+          font-size: ${({ theme }) => theme.texts.H7_TC.font_size};
+          line-height: ${({ theme }) => theme.texts.H7_TC.line_height};
+        }
+      }
+      .popup-active {
+        max-height: 109px;
+        padding: 14px 8px;
+        opacity: 1;
+      }
+    }
+  }
 `
 export default React.memo(StyledHeader)
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import StyledHeader from '../components/Header'
+import StyledFooter from '../components/Footer'
 import { Props } from 'utils/types'
 
 function Layout ({ className, children }: Props): React.ReactElement {
@@ -8,17 +9,44 @@ function Layout ({ className, children }: Props): React.ReactElement {
     <div className={className}>
       <StyledHeader><div>123</div></StyledHeader>
       <main className="Layout-main">{children}</main>
+      <StyledFooter />
     </div>
   )
 }
 
 const StyledLayout = styled(Layout)<Props>`
   background: ${({ theme }) => theme.colors.background};
+  .btn {
+    position: relative;
+    width: 40px;
+    height: 40px;
+    border-radius: 50px;
+    background: ${({ theme }) => theme.colors.background};
+    border-color: transparent;
+    box-shadow: ${({ theme }) => theme.effects.btn_shadow};
+    cursor: pointer;
+    &-icon {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    &-active {
+      background: ${({ theme }) => theme.colors.Gradual};
+      box-shadow: ${({ theme }) => theme.effects.btn_shadow2};
+      .btn-icon path {
+        stroke: ${({ theme }) => theme.colors.white};
+        stroke-width: 0.1px;
+        fill: ${({ theme }) => theme.colors.white};
+      }
+    }
+  }
   .Layout {
     &-main {
-      max-width: 1280px;
+      max-width: ${({ theme }) => theme.device.desktop};
       margin: 0 auto;
     }
+    
   }
 `
 
