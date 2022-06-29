84ab3be10a68017986ddd3b295262304e4b2c42f
YiCheng <49583905+YiChengChen0608@users.noreply.github.com>
2022-06-30 00:34:09 +0800

    Feat/api service (#7)
    * feat: add api service

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error'
+    camelcase: 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/camelcase': 'off'
   }
 }
